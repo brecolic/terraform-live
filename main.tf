fe2970e9efc883cd9008797b99872df87f12b24a
brecolic <101430508+brecolic@users.noreply.github.com>
2022-09-14 15:32:12 -0400

    adding dynamodb lock table

--- main.tf
+++ main.tf
@@ -16,6 +16,20 @@ terraform {
     region="us-east-1"
   }
 }
+  
+resource "aws_dynamodb_table" "terraform_lock" {
+    name           = "terraform-lock-table"
+    read_capacity  = 5
+    write_capacity = 5
+    hash_key       = "LockID"
+    attribute {
+        name = "LockID"
+        type = "S"
+    }
+    tags = {
+        "Name" = "State Lock Table"
+    }
+}
 
   
 
