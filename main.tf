78d9e19f9e718465db8e2644543af660dabde4bb
brecolic <101430508+brecolic@users.noreply.github.com>
2022-09-14 15:27:44 -0400

    updating bucket name

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket-from-registry-09q12831098309182430982309812039"
+  bucket = "my-s3-bucket-from-registry-0309182430982309812039-q0394-0afsdd"
   acl    = "private"
 
   versioning = {
